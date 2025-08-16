1af2e9e2ea2eca4ce9218258578e801c3dd6d7b6
amgskobo <211277585+amgskobo@users.noreply.github.com>
2025-08-16 12:32:31 +0900

    update

--- src/iqs7211e.c
+++ src/iqs7211e.c
@@ -768,7 +768,7 @@ static void iqs7211e_report_data(struct iqs7211e_data *data)
     data->touch_count++;
     // Layer switcher
     if (num_fingers != 0 &&
-        data->touch_count <= 3 &&
+        data->touch_count <= 4 &&
         config->scroll_layer > 0 &&
         !data->is_scroll_layer_active)
     {
@@ -885,7 +885,7 @@ static void iqs7211e_report_data(struct iqs7211e_data *data)
     int16_t smooth_dx = (dx + data->finger_1_prev_dx) >> 1;
     int16_t smooth_dy = (dy + data->finger_1_prev_dy) >> 1;
 
-    if (data->touch_count > 3)
+    if (data->touch_count > 4)
     {
         input_report_rel(data->dev, INPUT_REL_X, smooth_dx, false, K_NO_WAIT);
         input_report_rel(data->dev, INPUT_REL_Y, smooth_dy, true, K_NO_WAIT); // sync=true
